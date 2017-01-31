fd59118a00278772cb706a0018b077a56d79a90f
Unknown <daox008@gmail.com>
2017-01-31 13:32:12 +0100

    ^^

--- monMenu.py
+++ monMenu.py
@@ -3,63 +3,61 @@ import pygame
 
 pygame.init()
 
-class Item():
-    def __init__(self, color, text, width = 0, height = 0, (x, y) = (0, 0)):
-        self.color = color
-        self.text = text
-        self.rect = (x, y, width, height)
-        self.position = x, y
+class MenuItem():
+    def __init__(self, pos_x = 0, pos_y = 0):
+        self.image = pygame.image.load("Images/Menu/"+".png")
+        self.image = pygame.image.load("Images/Blanchon/b_idle_1.png")
+        self.width = self.image.get_rect().width
+        self.height = self.image.get_rect().height
+        self.dimensions = (self.width, self.height)
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.position = pos_x, pos_y
+        self.is_selected = False
 
     def set_position(self, x, y):
         self.position = (x, y)
         self.pos_x = x
         self.pos_y = y
 
-    def set_color(self, color):
-        self.color = color
+class GameMenu():
+    def __init__(self, screen, items, bg_color=(127,127,127)):
+        self.screen = screen
+        self.scr_width = self.screen.get_rect().width
+        self.scr_height = self.screen.get_rect().height
 
-    def get_position(self):
-        return self.position
+        self.bg_color = bg_color
+        self.clock = pygame.time.Clock()
 
-
-class Menu():
-    def __init__(self, window, items, color):
-        self.window = window
-        self.width = window.get_rect().width
-        self.height = window.get_rect().height
-        self.color = color
-        self.items = items
-        self.selectedItem = 0
         self.items = []
         for index, item in enumerate(items):
-            items = Item((255,255,255), item)
+            menu_item = MenuItem(item)
 
-            t_h = len(items) * items.height
-            pos_x = (self.width / 2) - (items.width / 2)
+            t_h = len(items) * menu_item.height
+            pos_x = (self.scr_width / 2) - (menu_item.width / 2)
+            pos_y = (self.scr_height / 2) - (t_h / 2) + ((index*2) + index * menu_item.height)
 
-            pos_y = (self.height / 2) - (t_h / 2) + ((index*2) + index * items.height)
+            menu_item.set_position(pos_x, pos_y)
+            self.items.append(menu_item)
 
-            items.set_position(pos_x, pos_y)
-            self.items.append(items)
+        self.cur_item = 1
 
-    def set_select_item(self, key):
-        for item in self.items:
-            item.set_color((255,255,255))
+    def set_item_selection(self, key):
 
-        if key == pygame.K_UP and self.selectedItem > 0:
-            self.selectedItem -= 1
-        elif key == pygame.K_UP and self.selectedItem == 0:
-            self.selectedItem = len(self.items) - 1
-        elif key == pygame.K_DOWN and self.selectedItem < len(self.items) - 1:
-            self.selectedItem += 1
-        elif key == pygame.K_DOWN and self.selectedItem == len(self.items) - 1:
-            self.selectedItem = 0
 
-        self.items[self.selectedItem].set_font_color(RED)
+        if key == pygame.K_UP and self.cur_item > 0:
+            self.cur_item -= 1
+        elif key == pygame.K_UP and self.cur_item == 0:
+            self.cur_item = len(self.items) - 1
+        elif key == pygame.K_DOWN and self.cur_item < len(self.items) - 1:
+            self.cur_item += 1
+        elif key == pygame.K_DOWN and self.cur_item == len(self.items) - 1:
+            self.cur_item = 0
+
 
     def run(self):
         while 1:
-            #self.clock.tick(60)
+            self.clock.tick(60)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -67,15 +65,20 @@ class Menu():
                 if event.type == pygame.KEYDOWN:
                     self.set_item_selection(event.key)
 
-            self.window.fill(self.color)
+
+            self.screen.fill(self.bg_color)
 
             for item in self.items:
-                self.window.blit(item.text, item.position)
+                self.screen.blit(item.image, item.position)
 
             pygame.display.flip()
 
+
 if __name__ == "__main__":
-    window = pygame.display.set_mode((1280, 720), 0, 32)
-    items = ('Jouer', 'Jouéje', 'Joueraje')
-    gm = Menu(window, items, (0,0,0))
+    screen = pygame.display.set_mode((640, 480), 0, 32)
+
+    menu_items = ("Jouer", "Option", "Crédits", "Quitter")
+
+    pygame.display.set_caption('Game Menu')
+    gm = GameMenu(screen, menu_items)
     gm.run()
