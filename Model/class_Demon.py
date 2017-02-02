a8f8a750666078166d8a5d276fc696151ef1bde2
Inspirateur <lehype75@gmail.com>
2017-02-02 13:09:08 +0100

    l'anim de mort du boss arrive

--- Model/class_Demon.py
+++ Model/class_Demon.py
@@ -18,29 +18,35 @@ class Demon(Mob):
                         "OdmgLeft":[pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_dmg.png").convert_alpha()))],
                         "OdmgRight":[pygame.transform.flip(pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_dmg.png").convert_alpha())), True, False)],
                         "Oaa1Left":[
-                                        pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged1_1.png").convert_alpha())),
-                                        pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged1_2.png").convert_alpha()))
+                                    pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged1_1.png").convert_alpha())),
+                                    pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged1_2.png").convert_alpha()))
                                     ],
                         "Oaa1Right":[
-                                        pygame.transform.flip(pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged1_1.png").convert_alpha())), True, False),
-                                        pygame.transform.flip(pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged1_2.png").convert_alpha())), True, False)
+                                    pygame.transform.flip(pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged1_1.png").convert_alpha())), True, False),
+                                    pygame.transform.flip(pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged1_2.png").convert_alpha())), True, False)
                                     ],
                         "Oaa3Left":[
-                                        pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged2_1.png").convert_alpha())),
-                                        pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged2_2.png").convert_alpha()))
+                                    pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged2_1.png").convert_alpha())),
+                                    pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged2_2.png").convert_alpha()))
                                     ],
                         "Oaa3Right":[
-                                        pygame.transform.flip(pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged2_1.png").convert_alpha())), True, False),
-                                        pygame.transform.flip(pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged2_2.png").convert_alpha())), True, False)
+                                    pygame.transform.flip(pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged2_1.png").convert_alpha())), True, False),
+                                    pygame.transform.flip(pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged2_2.png").convert_alpha())), True, False)
                                     ],
                         "Oaa2Left":[
-                                        pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged3_1.png").convert_alpha())),
-                                        pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged3_2.png").convert_alpha()))
+                                    pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged3_1.png").convert_alpha())),
+                                    pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged3_2.png").convert_alpha()))
                                     ],
                         "Oaa2Right":[
-                                        pygame.transform.flip(pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged3_1.png").convert_alpha())), True, False),
-                                        pygame.transform.flip(pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged3_2.png").convert_alpha())), True, False)
-                                    ]}
+                                    pygame.transform.flip(pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged3_1.png").convert_alpha())), True, False),
+                                    pygame.transform.flip(pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_atkranged3_2.png").convert_alpha())), True, False)
+                                    ],
+                        "DRight":[
+                                    pygame.transform.flip(pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_dead.png").convert_alpha())), True, False)
+                                ],
+                        "DLeft":[
+                                    pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load("Images/Boss/boss_dead.png").convert_alpha()))
+                                ]}
         atkList = [Atk("feu", 2, 32, 16, {"idleRight":[pygame.transform.scale2x(pygame.image.load("Images/Boss/fire_1.png").convert_alpha()),
                                                         pygame.transform.scale2x(pygame.image.load("Images/Boss/fire_2.png").convert_alpha())],
                                             "idleLeft":[pygame.transform.flip(pygame.transform.scale2x(pygame.image.load("Images/Boss/fire_1.png").convert_alpha()),True,False),
@@ -61,6 +67,8 @@ class Demon(Mob):
         self.states["Oaa2Left"] = 200
         self.states["Oaa3Right"] = 200
         self.states["Oaa3Left"] = 200
+        self.states["DRight"] = 200
+        self.states["DLeft"] = 200
         self.strength = strength
         self.spellMax = 5 + int(strength)
         self.spellCount = 0
@@ -145,6 +153,18 @@ class Demon(Mob):
                         self.flee_y = (pygame.time.get_ticks()%(self.max_y-self.min_y))+self.min_y
 
         Mob.update(self, fps)
+
+    def set_hp(self, dmg):
+        Charac.set_hp(self, dmg)
+        if(self.hp <= 0.0):
+            if(self.facing == 1):
+                Animated.changeState(self, "DRight")
+            else:
+                Animated.changeState(self, "DLeft")
+            self.baseJumpForce = 0
+            self.speed_x = 0
+            self.baseAcc_x = 0
+            self.currAcc_x = 0
 
     def testAtkEffect(self, atkEffect):
         temp_speed_x = self.speed_x
